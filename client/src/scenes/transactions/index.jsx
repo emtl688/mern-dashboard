6955ead5be633e227b2fe65272fce43bb7544c29
emtl688 <ericpmoran1@gmail.com>
2023-02-10 15:35:22 -0500

    Transactions page placeholder

---
+++ client/src/scenes/transactions/index.jsx
@@ -0,0 +1,14 @@
+import Header from 'components/Header'
+import React from 'react'
+import { Box } from '@mui/material'
+
+const Transactions = () => {
+    return (
+        <Box m="1.5rem 2.5rem">
+            <Header title="TRANSACTIONS" subtitle="List of Transactions"/>
+            <Box></Box>
+        </Box>
+    )
+}
+
+export default Transactions;
